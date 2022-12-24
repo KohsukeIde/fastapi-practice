070167b6a263e0d0b07f8c922d051d8293cd39f7
KohsukeIde <kohsukeide0726@gmail.com>
2022-12-25 05:50:07 +0900

    New: operation description

--- main.py
+++ main.py
@@ -1,34 +1,57 @@
-from fastapi import FastAPI
-from enum import Enum
 from typing import Optional
+from fastapi import FastAPI, Response, status
+from enum import Enum
+
 
 app = FastAPI()
 
-@app.get("/hello")
+@app.get('/hello')
 def index():
-    return {"message" : "Hello World"}
+  return {'message': 'Hello world!'}
 
 # @app.get('/blog/all')
-# def get_blog():
-#     return {'message' : f'all blogs provided'}
+# def get_all_blogs():
+#   return {'message': 'All blogs provided'}
 
-@app.get('/blog/all')
-def get_all_blog(page=1, page_size: Optional[int] = None):
-    return {'message' : f'All {page_size} blogs on page {page}'}
+@app.get(
+    '/blog/all',
+    tags=['blog'],
+    summary='Retrieve all blogs',
+    description='This api calls simulates fetching all blogs',
+    response_description='The list of available blogs',
+    )
+def get_blogs(page = 1, page_size: Optional[int] = None):
+  return {'message': f'All {page_size} blogs on page {page}'}
 
-@app.get('/blog/{id}/comments/{comment_id}')
-def get_comments(id: int, comment_id: int, valid: bool = True, username: Optional[str] = None):
-    return {'message' : f'Blog id {id}, comment id {comment_id}, valid {valid}, username {username}'}
+@app.get('blog/{id}/comments/{comment_id}', tags=['blog', 'comment'])
+def get_comment(id: int, comment_id: int, valid: bool = True, username: Optional[str] = None):
+    """simulates retrieving a comment of a blog
+
+    Args:
+    - **id** (int): mandatory path parameter
+    - **comment_id** (int): mandatory path parameter
+    - **valid** (bool, optional):optional query parameter. Defaults to True.
+    - **username** (Optional[str], optional): ptional query parameter Defaults to None.
+
+    Returns:
+        str: info regarding the comment
+    """
+    return {'message': f'blog_id {id}, comment_id {comment_id}, valid {valid}, username {username}'}
 
 class BlogType(str, Enum):
     short = 'short'
     story = 'story'
     howto = 'howto'
-    
-@app.get('/blog/type/{type}')
-def get_blog_type(type: BlogType):
-    return {'message' : f'Blog type {type}'}
 
-@app.get('/blog/{id}')
-def get_blog(id : int):
-    return {'message' : f'Blog with id {id}'}
+@app.get('/blog/type/{type}', tags=['blog'])
+def get_blog_type(type: BlogType):
+  return {'message': f'Blog type {type}'}
+
+@app.get('/blog/{id}', status_code=status.HTTP_200_OK, tags=['blog'])
+def get_blog(id: int, response: Response):
+  if id > 5:
+    response.status_code = status.HTTP_404_NOT_FOUND
+    return {'error': f'Blog {id} not found'}
+  else : 
+    response.status_code = status.HTTP_200_OK
+    return {'message': f'Blog with id {id}'}
